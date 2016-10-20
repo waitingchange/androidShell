e0b092fb45c8e21c3b28a62bffa2e9148009935e
MacBook Pro <change@MacBook-Pro.local>
2016-10-20 17:49:39 +0800

    图片下载

--- downLoadTest/app/src/main/java/com/example/wangjunpeng/downloadtest/MainActivity.java
+++ downLoadTest/app/src/main/java/com/example/wangjunpeng/downloadtest/MainActivity.java
@@ -1,135 +1,206 @@
 package com.example.wangjunpeng.downloadtest;
 
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.os.Environment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-    public static final String LOG_TAG = "test";
+// downLoad Imag
 
-    private ProgressDialog mProgressDialog;
-    public static final int DIALOG_DOWNLOAD_PROGRESS = 0;
-
-
-    File rootDir = Environment.getExternalStorageDirectory();
-
-    //定义要下载的文件名
-    public String fileName = "test.jpg";
-    public String fileURL = "http://scimg.jb51.net/allimg/160815/103-160Q509544OC.jpg";
-
-
-
-
+public class MainActivity extends Activity {
+    Button visitWebBtn = null;
+    Button downImgBtn = null;
+    TextView showTextView = null;
+    ImageView showImageView = null;
+    String resultStr = "";
+    ProgressBar progressBar = null;
+    ViewGroup viewGroup = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //检查下载目录是否存在
-        checkAndCreateDirectory("/mydownloads");
+        showTextView = (TextView)findViewById(R.id.textview_show);
+        showImageView = (ImageView)findViewById(R.id.imagview_show);
+        downImgBtn = (Button)findViewById(R.id.button);
 
-        //执行asynctask
-        new DownloadFileAsync().execute(fileURL);
 
-    }
+        downImgBtn.setOnClickListener(new View.OnClickListener() {
 
-    public void checkAndCreateDirectory(String dirName){
-        File new_dir = new File( rootDir + dirName );
-        if( !new_dir.exists() ){
-            Log.d(LOG_TAG, "checkAndCreateDirectory =======  " + rootDir + dirName);
-            new_dir.mkdirs();
-        }
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                showImageView.setVisibility(View.VISIBLE);
+                showTextView.setVisibility(View.GONE);
+                String imgUrl = "http://www.howtogeek.com/wp-content/uploads/2015/09/650x300xht1-650x300.png.pagespeed.gp+jp+jw+pj+js+rj+rp+rw+ri+cp+md.ic.tn4-tb9EzM.png";
+                new DownImgAsyncTask().execute(imgUrl);
+            }
+        });
     }
 
     @Override
-    protected Dialog onCreateDialog(int id) {
-        switch (id) {
-            case DIALOG_DOWNLOAD_PROGRESS: //we set this to 0
-                mProgressDialog = new ProgressDialog(this);
-                mProgressDialog.setMessage("Downloading file...");
-                mProgressDialog.setIndeterminate(false);
-                mProgressDialog.setMax(100);
-                mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                mProgressDialog.setCancelable(true);
-                mProgressDialog.show();
-                return mProgressDialog;
-            default:
-                return null;
-        }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
-    class DownloadFileAsync extends AsyncTask<String, String, String> {
+    /**
+     * 获取指定URL的响应字符串
+     * @param urlString
+     * @return
+     */
+    private String getURLResponse(String urlString){
+        HttpURLConnection conn = null; //连接对象
+        InputStream is = null;
+        String resultData = "";
+        try {
+            URL url = new URL(urlString); //URL对象
+            conn = (HttpURLConnection)url.openConnection(); //使用URL打开一个链接
+            conn.setDoInput(true); //允许输入流，即允许下载
+            conn.setDoOutput(true); //允许输出流，即允许上传
+            conn.setUseCaches(false); //不使用缓冲
+            conn.setRequestMethod("GET"); //使用get请求
+            is = conn.getInputStream();   //获取输入流，此时才真正建立链接
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader bufferReader = new BufferedReader(isr);
+            String inputLine  = "";
+            while((inputLine = bufferReader.readLine()) != null){
+                resultData += inputLine + "\n";
+            }
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            if(is != null){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            if(conn != null){
+                conn.disconnect();
+            }
+        }
+
+        return resultData;
+    }
+
+    /**
+     * 从指定URL获取图片
+     * @param url
+     * @return
+     */
+    private Bitmap getImageBitmap(String url){
+        URL imgUrl = null;
+        Bitmap bitmap = null;
+        try {
+            imgUrl = new URL(url);
+            HttpURLConnection conn = (HttpURLConnection)imgUrl.openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
+    class VisitWebRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            // TODO Auto-generated method stub
+            String data = getURLResponse("http://www.baidu.com/");
+            resultStr = data;
+        }
+
+    }
+    class DownImgAsyncTask extends AsyncTask<String, Void, Bitmap>{
+
 
         @Override
         protected void onPreExecute() {
+            // TODO Auto-generated method stub
             super.onPreExecute();
-            showDialog(DIALOG_DOWNLOAD_PROGRESS);
+            showImageView.setImageBitmap(null);
+            showProgressBar();//显示进度条提示框
+
         }
 
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            Bitmap b = getImageBitmap(params[0]);
+            return b;
+        }
 
         @Override
-        protected String doInBackground(String... aurl) {
-
-            try {
-                //连接地址
-                URL u = new URL(fileURL);
-                HttpURLConnection c = (HttpURLConnection) u.openConnection();
-                c.setRequestMethod("GET");
-                c.setDoOutput(true);
-                c.connect();
-
-                //计算文件长度
-                int lenghtOfFile = c.getContentLength();
-
-                File pic = new File(rootDir + "/my_downloads/", fileName);
-                Log.d(LOG_TAG, "aaaaaaaaaa " + pic.exists());
-                System.out.print("aaaaaaaaaa " + pic.exists());
-
-                FileOutputStream f = new FileOutputStream(pic);
-
-                InputStream in = c.getInputStream();
-
-                //下载的代码
-                byte[] buffer = new byte[1024];
-                int len1 = 0;
-                long total = 0;
-
-                while ((len1 = in.read(buffer)) > 0) {
-                    total += len1; //total = total + len1
-                    publishProgress("" + (int)((total*100)/lenghtOfFile));
-                    f.write(buffer, 0, len1);
-                }
-                f.close();
-
-            } catch (Exception e) {
-                Log.d(LOG_TAG, e.getMessage());
+        protected void onPostExecute(Bitmap result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            if(result!=null){
+                dismissProgressBar();
+                showImageView.setImageBitmap(result);
             }
-
-            return null;
         }
 
-        protected void onProgressUpdate(String... progress) {
-            Log.d(LOG_TAG,progress[0]);
-            mProgressDialog.setProgress(Integer.parseInt(progress[0]));
-        }
-
-        @Override
-        protected void onPostExecute(String unused) {
-            //dismiss the dialog after the file was downloaded
-            dismissDialog(DIALOG_DOWNLOAD_PROGRESS);
-        }
 
 
     }
+    /**
+     * 在母布局中间显示进度条
+     */
+    private void showProgressBar(){
+        progressBar = new ProgressBar(this, null, android.R.attr.progressBarStyleLarge);
+//        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+//                ViewGroup.LayoutParams.WRAP_CONTENT);
+//        params.addRule(RelativeLayout.CENTER_IN_PARENT,  RelativeLayout.TRUE);
+//        progressBar.setVisibility(View.VISIBLE);
+//        Context context = getApplicationContext();
+//        viewGroup = (ViewGroup)findViewById(R.id.parent_view);
+////              MainActivity.this.addContentView(progressBar, params);
+//        viewGroup.addView(progressBar, params);
+    }
+    /**
+     * 隐藏进度条
+     */
+    private void dismissProgressBar(){
+//        if(progressBar != null){
+//            progressBar.setVisibility(View.GONE);
+//            viewGroup.removeView(progressBar);
+//            progressBar = null;
+//        }
+    }
+
 }
+
